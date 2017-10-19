cc3cdad7e1f5b242f3acc6e2480df8e7f511a6e2
evaccaro <erv230@nyu.edu>
2017-10-19 11:13:16 -0400

    finished challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,43 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.select do |rest|
+      rest.name == name
+    end.first
+  end
+
+  def reviews
+    Review.all.select do |rev|
+      rev.restaurant == self
+    end
+  end
+
+  def customers
+    self.reviews.map do |rev|
+      rev.customer
+    end
   end
 
 end
+
+# Build out the following methods on the restaurant class
+#
+# + Restaurant.all
+#   + returns an array of all restaurants
+# + Restaurant.find_by_name(name)
+#   + given a string of restaurant name, returns the first restaurant that matches
+# + Restaurant#reviews
+#   + returns an array of all reviews for that restaurant
+# + Restaurant#customers
+#   + should return all of the customers who have written reviews of that restaurant.
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,29 @@
 class Review
-  
+
+attr_accessor :customer, :restaurant, :content
+@@all = []
+
+  def initialize(customer, restaurant, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
 
+
+
+# Build out the following methods on the Review class
+#
+# + Review.all
+#   + returns all of the reviews
+# + Review#customer
+#   + returns the customer for that given review
+# + Review#restaurant
+#   + returns the restaurant for that given review
