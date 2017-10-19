37c55379fc431a4fca74d0a049c60b24b98e0e08
Liam Killion <liam.killion@gmail.com>
2017-10-19 11:12:57 -0400

    code challenge response Liam K.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,43 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all<<self
+  end
+  # Restaurant.all
+  # returns an array of all restaurants
+  def self.all
+    @@all
+  end
+  # Restaurant.find_by_name(name)
+  # given a string of restaurant name, returns the first restaurant that matches
+  def self.find_by_name(name)
+    Restaurant.all.find { |restaurant| restaurant.name == name  }
+  end
+  # Restaurant#reviews
+  # returns an array of all reviews for that restaurant
+  def reviews
+    Review.all.map do |review|
+      if review.restaurant = self
+        review.content
+      end
+    end
+  end
+  # Restaurant#customers
+  # should return all of the customers who have written reviews of that restaurant.
+  def customers
+    Review.all.map do |review|
+      if review.restaurant = self
+        review.customer
+      end
+    end
   end
 
+  def avg_rating
+    rating_array = Review.all.map { |review| review.rating  }
+    rating_array.inject(0.0) { |sum,el| sum + el } / rating_array.size
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,24 @@
 class Review
-  
-end
+    # Review#customer
+    # returns the customer for that given review
+    # Review#restaurant
+    # returns the restaurant for that given review
+  attr_accessor :customer, :restaurant, :content, :rating
 
+  @@all = []
+
+  def initialize(restaurant, content, rating)
+    @restaurant = restaurant
+    @content = content
+    @@all<<self
+    @rating = rating
+    self.restaurant=restaurant
+
+  end
+  # Review.all
+  # returns all of the reviews
+  def self.all
+    @@all
+  end
+
+end
