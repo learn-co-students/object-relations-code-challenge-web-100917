6299cb524c0ae58b1c3faca5d2c6bab2000c7abf
flored27 <dflores@fandm.edu>
2017-10-19 11:17:44 -0400

    Diego

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,41 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(nombre)
+    @@all.find do |n|
+      if n.name == nombre
+         n
+      end
+    end
+  end
+
+  def reviews
+    all_reviews = []
+    Review.all.each do |rev|
+      if rev.restaurant == self.name
+        all_reviews << rev
+      end
+    end
+    all_reviews
+  end
+
+  def customers
+    all_customers = []
+    restaurant.reviews.each do |rev|
+      all_customers << rev.customer
+    end
+    all_customers
   end
 
 end
