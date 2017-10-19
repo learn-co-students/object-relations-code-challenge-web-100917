b0db1b42c02c77ef3eda0ea84fa30db9edf83244
Brandon Harris <cheeridon@gmail.com>
2017-10-19 11:14:57 -0400

    finished challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,24 @@
 class Restaurant
-  attr_accessor :name
-
+  attr_accessor :name, :reviews
+  @@restaurants =[]
   def initialize(name)
     @name = name
+    @@restaurants << self
   end
 
+  def self.all
+    @@restaurants
+  end
+
+  def self.find_by_name name
+    @@restaurants.find{|restaurant| restaurant.name == name}.name
+  end
+
+  def reviews
+    Review.all.select{|review| review.content if review.restaurant == self}.content
+  end
+
+  def customers
+    Review.all.map{|review| review.customer}.uniq
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review
-  
-end
+  attr_accessor :customer, :restaurant, :content
+  @@reviews = []
+  def initialize(customer, restaurant, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@reviews << self
+  end
 
+  def self.all
+    @@reviews
+  end
+end
