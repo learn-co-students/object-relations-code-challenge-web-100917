6d1cf43e35c7a4ab6c3617e250210dad2e63148c
MatteBru <mbruni616@gmail.com>
2017-10-19 11:16:59 -0400

    Matteo Finished Challenge.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,32 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|rest| rest.name == name}
+  end
+
+  def customers
+    self.reviews.map {|rev| rev.customer }
+  end
+
+  def competitors
+    comp = self.customers.map{|cust| cust.restaurants}.flatten.uniq
+    comp.delete_if {|rest| rest == self }
+  end
+
+  def reviews
+    Review.all.select{|rev| rev.restaurant == self}
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
 class Review
-  
-end
 
+  @@all = []
+
+  attr_accessor :content, :restaurant, :customer
+
+  def initialize(restaurant, customer, content)
+    @restaurant = restaurant
+    @customer = customer
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+end
