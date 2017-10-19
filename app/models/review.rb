417bc23d6445b2a06d508eaa6e8084fedb0ce28e
Abe Rudy <aberudy@gmail.com>
2017-10-19 11:13:07 -0400

    completed methods for code challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
-end
 
+  @@all = []
+
+  attr_accessor :customer, :restaurant, :content
+
+  def initialize(customer, restaurant, content) #working
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,15 @@ def reload
   load 'config/environment.rb'
 end
 
+abe = Customer.new("Abe", "Rudy")
+pizza = Restaurant.new("Wheated")
+test = Review.new(abe, pizza, "Hey there!")
+
+shirley = Customer.new("Shirley", "Lin")
+burger = Restaurant.new("Burgers")
+test1 = Review.new(shirley, burger, "hello!")
+
+brandon = Customer.new("Brandon", "Harris")
+test2 = brandon.add_review(pizza, "AWESOME")
+
 Pry.start
