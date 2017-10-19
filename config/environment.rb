dc49b65d1ed0029630ff57b61f384a0b74ea05ac
yassimortensen <yassimortensen@gmail.com>
2017-10-19 11:13:46 -0400

    submit code challenge mod 1

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,9 @@
 require 'bundler/setup'
 Bundler.require
 require_all 'app'
+
+yassi = Customer.new("Yassi", "Mortensen")
+ayis = Customer.new("Ayis", "Megiris")
+
+italian = Restaurant.new("Olive")
+greek = Restaurant.new("Souvla")
