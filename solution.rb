cd0a161f7f8f991d90fe991316399b4ce5820681
github username <github email address>
2017-10-19 11:13:11 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,1 +1,117 @@
 # Please copy/paste all three classes into this file to submit your solution!
+class Customer
+  attr_accessor :first_name, :last_name
+
+  @@all = []
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @@all << self
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |customer|
+      name == customer.full_name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select do |customer|
+      name == customer.first_name
+    end
+  end
+
+  def self.all_names
+    self.all.collect do |customer|
+      customer.full_name
+    end
+  end
+
+  def add_review(restaurant, content)
+    Review.new(content, self, restaurant)
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def restaurants
+    self.reviews.collect do |review|
+      review.restaurant
+    end
+  end
+
+end
+
+class Restaurant
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    self.reviews.collect do |review|
+      review.customer
+    end
+  end
+
+end
+
+class Review
+
+  attr_accessor :content, :customer, :restaurant
+
+  @@all = []
+
+  def initialize(content, customer, restaurant)
+    @content = content
+    @customer = customer
+    @restaurant = restaurant
+    @@all << self
+  #  customer.add_review(restaurant, content)
+  end
+
+  def customer
+    self.customer
+  end
+
+  def restaurant
+    self.restaurant
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,23 @@ def reload
   load 'config/environment.rb'
 end
 
+#customers
+winter = Customer.new("Winter", "LaMon")
+james = Customer.new("James", "Graham")
+jordan = Customer.new("Jordan", "Storms")
+dakota = Customer.new("Dakota", "Lillie")
+
+#restaurants
+
+shake_shack = Restaurant.new("Shake Shack")
+toms_restaurant = Restaurant.new("Tom's Restaurant")
+olive_garden = Restaurant.new("Olive Garden")
+
+#reviews
+review_1 = Review.new("Yum!", winter, "Shake Shack")
+review_2 = Review.new("Amazing pancakes!", jordan, "Tom's Restaurant")
+review_3 = Review.new("Only go here for the unlimited breadsticks.", james, "Olive Garden")
+review_4 = Review.new("I've died and gone to pancake heaven.", winter, "Tom's Restaurant")
+review_5 = Review.new("Best burgers on the east coast!", dakota, "Shake Shack")
+
 Pry.start
