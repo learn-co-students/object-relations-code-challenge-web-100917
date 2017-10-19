0154d868ac6593d11fd82317b81bc08614c19fcd
github username <github email address>
2017-10-19 11:13:19 -0400

    Solutions to Ruby Code Challenge

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,15 @@ def reload
   load 'config/environment.rb'
 end
 
+jerry = Customer.new("Jerry", "Seinfield")
+george = Customer.new("Jerry", "Lewis")
+elaine = Customer.new("Elaine", "Benes")
+cosmo = Customer.new("Cosmo", "Kramer")
+
+bobs = Restaurant.new("Bobs Burgers")
+fuku = Restaurant.new("Fuku")
+
+review1 = Review.new(jerry, bobs, "Cool")
+review2 = Review.new(elaine, fuku, "Yum")
+
 Pry.start
